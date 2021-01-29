baca17703d6cff4c10eb1136b9bc4d81e4943153
Jota Farias <fariasjota09@gmail.com>
2021-01-28 22:22:09 -0300

    Primeiro commit usando o magit.

--- init.el
+++ init.el
@@ -32,8 +32,7 @@
 (setq package-enable-at-startup nil) ; desabilitar início de ativação
 
 ; Repositório MELPA
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize) ; inicializar pacotes
 
 ; Instalação do use-package
@@ -42,26 +41,6 @@
   (package-install 'use-package))
 
 ; Adicionado automaticamente pelo MELPA
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
- '(custom-enabled-themes '(use-package))
- '(custom-safe-themes
-   '("2dff5f0b44a9e6c8644b2159414af72261e38686072e063aa66ee98a2faecf0e" "8f567db503a0d27202804f2ee51b4cd409eab5c4374f57640317b8fcbbd3e466" default))
- '(package-selected-packages
-   '(yasnippet-snippets yasnippet flycheck ace-window all-the-icons neotree which-key molokai-theme try use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 
 
 ;; Pacote Try
@@ -80,7 +59,7 @@
 
 ; (use-package dracula-theme ; tema dracula
 ;   :ensure t
-;   :config (load-theme 'dracula))
+;   :config (load-theme 'dracula t))
 
 (use-package vscode-dark-plus-theme ; tema vscode dark+
   :ensure t
@@ -151,5 +130,23 @@
 ; M-x package-install RET yasnippet-snippets
 
 
+;; Pacote exec-path-from-shell
+(use-package exec-path-from-shell ; torna o PATH do shell igual do temrinal
+  :ensure t)
+(when (memq window-system '(mac ns x))
+  (exec-path-from-shell-initialize))
 
+
+;; Pacote Magit
+(use-package magit
+  :ensure t)
+
+
+;; Diminuir prompts yes/no
+(fset 'yes-or-no-p 'y-or-n-p)
+
+
+;; Enviar comandos custom para outro arquivo
+;(setq custom-file "~/.emacs.d/etc/custom.el")
+;(load custom-file)
 
